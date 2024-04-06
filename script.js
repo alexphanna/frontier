a2cbc8e5f1f0587537b6fcd6af6e7f64448c7274
Alex Hanna <ahanna0608@gmail.com>
2024-04-06 01:26:22 -0400

    Add Building

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 class Tile {
-    constructor(name, x, y, sideLength, svg) {
+    constructor(name, number, x, y, sideLength, svg) {
         this.name = name;
+        this.number = number;
         this.x = x;
         this.y = y;
         this.sideLength = sideLength;
@@ -12,7 +13,7 @@ class Tile {
         return (Math.sqrt(3) / 2) * this.sideLength;
     }
     draw() {
-        let hexagon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+        let hexagon = createHexagon(this.x, this.y, this.sideLength);
         switch (this.name) {
             case "Brick":
                 hexagon.setAttribute("fill", "brown");
@@ -34,83 +35,92 @@ class Tile {
                 break;
         }
         hexagon.setAttribute("stroke", "black");
-        hexagon.setAttribute("stroke-width", "2");
-        let points = "";
-        for (let i = 0; i < 6; i++) {
-            const angle = 2 * Math.PI / 6 * i - Math.PI / 2;
-            const x = this.x + this.sideLength * Math.cos(angle);
-            const y = this.y + this.sideLength * Math.sin(angle);
-            points += `${x},${y} `;
-        }
-        hexagon.setAttribute("points", points);
+        hexagon.setAttribute("stroke-width", "1");
+        hexagon.onmousedown = () => {
+            console.log(this.name);
+        };
         this.svg.appendChild(hexagon);
+
+        if (this.name == "Desert") return;
+
+        let token = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        token.setAttribute("cx", this.x);
+        token.setAttribute("cy", this.y);
+        token.setAttribute("r", this.sideLength / 2.5);
+        token.setAttribute("fill", "tan");
+        token.setAttribute("filter", "drop-shadow(1px 1px 1px rgb(0 0 0 / 1))");
+        this.svg.appendChild(token);
 
         let text = document.createElementNS("http://www.w3.org/2000/svg", "text");
         text.setAttribute("x", this.x);
-        text.setAttribute("y", this.y + this.sideLength / 2);
-        text.setAttribute("text-anchor", "middle");
-        text.setAttribute("dominant-baseline", "middle");
-        text.setAttribute("font-size", "12.5");
-        text.textContent = this.name;
-        this.svg.appendChild(text);
-
-        text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-        text.setAttribute("x", this.x);
         text.setAttribute("y", this.y);
-        text.setAttribute("text-anchor", "middle");
-        text.setAttribute("dominant-baseline", "middle");
-        text.setAttribute("font-size", "30");
-        text.textContent = Math.floor(Math.random() * 11 + 2);
+        text.setAttribute("fill", (this.number == 6 || this.number == 8 ? "red" : "black"));
+        text.setAttribute("font-size", "25");
+        text.textContent = this.number;
         this.svg.appendChild(text);
     }
 }
 
-let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-svg.setAttribute("width", "1000");
-svg.setAttribute("height", "1000");
+function generateMap(width) {
+    const resourceCounts = {
+        "Brick": 3,
+        "Wool": 4,
+        "Ore": 3,
+        "Grain": 4,
+        "Lumber": 4
+    };
+    const resources = Object.keys(resourceCounts);
+    const distr = resources.flatMap(resource => Array(resourceCounts[resource]).fill(resources.indexOf(resource)));
+    distr.push(6);
+    shuffle(distr);
 
-const resources = ["Brick", "Wool", "Ore", "Grain", "Lumber"];
+    const map = [];
+    for (let i = 3; i <= width; i++) {
+        const row = [];
+        for (let j = 0; j < i; j++) {
+            row.push(distr.pop());
+        }
+        map.push(row);
+    }
+    for (let i = width - 1; i >= 3; i--) {
+        const row = [];
+        for (let j = 0; j < i; j++) {
+            row.push(distr.pop());
+        }
+        map.push(row);
+    }
+    console.log(map);
+    return map;
+}
 
-drawMap(5, svg);
-
-/**
- * Draws a map of tiles with the given width.
- * 
- * @param {number} width - The width of the map.
- * @param {SVGElement} svg - The SVG element to draw the map on.
- */
-function drawMap(width, svg) {
+function drawMap(map, svg) {
     const sideLength = 50;
     const inradius = (Math.sqrt(3) / 2) * sideLength;
-    const resourceDistribution = distributeResources();
-    let n = 0;
-    
-    for (let i = 3; i <= width; i++) {
-        for (let j = 0; j < i; j++) {
-            new Tile(resourceDistribution[n], 100 + inradius * 2 * j + (inradius * (width - i)), 100 + sideLength * (i - 3) * 1.5, sideLength, svg);
-            n++;
-            if (i < width) {
-                new Tile(resourceDistribution[n], 100 + inradius * 2 * j + (inradius * (width - i)), 100 + 75 * width + sideLength * (width - i - 3) * 1.5, sideLength, svg);
-                n++;
-            }
+
+    let maxLength = 0;
+    for (let i = 0; i < map.length; i++) {
+        if (map[i].length > maxLength) maxLength = map[i].length;
+    }
+
+    let distr = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12];
+    distr = shuffle(distr);
+
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[i].length; j++) {
+            new Tile((map[i][j] == 6 ? "Desert" : resources[map[i][j]]), (map[i][j] == 6 ? "" : distr.pop()), 100 + inradius * 2 * j + (maxLength - map[i].length) * inradius, 100 + sideLength * 1.5 * i, sideLength, svg);
         }
     }
 }
 
-function distributeResources() {
-    let distribution = [];
-    for (let resource of resources) {
-        let n = 4;
-        if (resource == "Brick" || resource == "Ore") {
-            n = 3;
-        }
-        for (let j = 0; j < n; j++) {
-            distribution.push(resource);
-        }
+function createHexagon(x, y, sideLength) {
+    let hexagon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    let points = "";
+    for (let i = 0; i < 6; i++) {
+        const angle = 2 * Math.PI / 6 * i - Math.PI / 2;
+        points += `${x + sideLength * Math.cos(angle)},${y + sideLength * Math.sin(angle)} `;
     }
-    distribution.push("Desert");
-
-    return shuffle(distribution);
+    hexagon.setAttribute("points", points);
+    return hexagon;
 }
 
 /**
@@ -131,5 +141,66 @@ function shuffle(array) {
 
     return array;
 }
+
+class Building {
+    constructor(svg) {
+        this.type = "Settlement";
+        this.svg = svg;
+
+        this.draw();
+    }
+    draw() {
+        let building = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        building.setAttribute("id", "building");
+        building.setAttribute("x", "100");
+        building.setAttribute("y", "100");
+        building.setAttribute("width", "20");
+        building.setAttribute("height", "10");
+        building.setAttribute("fill", "blue");
+        building.setAttribute("stroke", "black");
+        building.setAttribute("stroke-width", "1");
+        dragElement(building);
+        this.svg.appendChild(building);
+    }
+}
+
+function dragElement(element) {
+    element.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e.preventDefault();
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e.preventDefault();
+        element.setAttribute("x", e.clientX);
+        element.setAttribute("y", e.clientY);
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+svg.setAttribute("width", "1000");
+svg.setAttribute("height", "1000");
+
+const resources = ["Brick", "Wool", "Ore", "Grain", "Lumber"];
+
+const map = [
+    [1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1],
+];
+
+drawMap(generateMap(5), svg);
+
+new Building(svg);
 
 document.body.appendChild(svg);
