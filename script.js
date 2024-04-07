4db3423d2249532c5f875d1ad39c86dcd990fb36
Alex Hanna <ahanna0608@gmail.com>
2024-04-07 02:06:31 -0400

    Add player

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+class Player {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+        this.resources = {
+            "Brick": 5,
+            "Wool": 5,
+            "Ore": 0,
+            "Grain": 5,
+            "Lumber": 5
+        };
+        this.settlements = 5;
+        this.cities = 4;
+        this.roads = 15;
+        this.victoryPoints = 0;
+
+        this.updateInfo();
+    }
+    get canBuildSettlement() {
+        return this.resources["Brick"] >= 1 && this.resources["Wool"] >= 1 && this.resources["Grain"] >= 1 && this.resources["Lumber"] >= 1 && this.settlements > 0;
+    }
+    get canBuildCity() {
+        return this.resources["Ore"] >= 3 && this.resources["Grain"] >= 2 && this.cities > 0;
+    }
+    updateInfo() {
+        document.getElementById("resources").innerHTML = `Brick: ${this.resources["Brick"]}<br>Wool: ${this.resources["Wool"]}<br>Ore: ${this.resources["Ore"]}<br>Grain: ${this.resources["Grain"]}<br>Lumber: ${this.resources["Lumber"]}`;
+        document.getElementById("buildings").innerHTML = `Settlements: ${this.settlements}<br>Cities: ${this.cities}<br>Roads: ${this.roads}`;
+        if (!this.canBuildSettlement) {
+            document.getElementById("buildSettlement").disabled = true;
+        }
+        if (!this.canBuildCity) {
+            document.getElementById("buildCity").disabled = true;
+        }
+    }
+    buildSettlement() {
+        if (this.canBuildSettlement) {
+            this.resources["Brick"]--;
+            this.resources["Wool"]--;
+            this.resources["Grain"]--;
+            this.resources["Lumber"]--;
+            this.settlements--;
+            this.victoryPoints++;
+            this.updateInfo();
+            new Building(100, 100, this.color, "Settlement", svg);
+        }
+    }
+    buildCity() {
+        if (this.canBuildCity) {
+            this.resources["Ore"] -= 3;
+            this.resources["Grain"] -= 2;
+            this.cities--;
+            this.victoryPoints++;
+            this.updateInfo();
+            new Building(100, 100, this.color, "City", svg);
+        }
+    }
+}
+
 class Tile {
     constructor(name, number, x, y, sideLength, svg) {
         this.name = name;
@@ -9,9 +67,11 @@ class Tile {
 
         this.draw();
     }
+
     get inradius() {
         return (Math.sqrt(3) / 2) * this.sideLength;
     }
+
     draw() {
         let group = document.createElementNS("http://www.w3.org/2000/svg", "g");
         group.id = "Tile";
@@ -43,11 +103,6 @@ class Tile {
                 hexagon.setAttribute("fill", "#ffd080");
                 break;
         }
-        hexagon.setAttribute("stroke", "black");
-        hexagon.setAttribute("stroke-width", "2");
-        hexagon.onmousedown = () => {
-            console.log(this.name);
-        };
         group.appendChild(hexagon);
 
         if (this.name != "Desert") {
@@ -101,7 +156,6 @@ function generateMap(width) {
         }
         map.push(row);
     }
-    console.log(map);
     return map;
 }
 
@@ -134,7 +188,6 @@ function createHexagon(x, y, sideLength) {
         vertices += `${x + sideLength * Math.cos(angle)},${y + sideLength * Math.sin(angle)} `;
     }
     hexagon.setAttribute("points", vertices);
-    console.log(vertices);
     return hexagon;
 }
 
@@ -158,17 +211,19 @@ function shuffle(array) {
 }
 
 class draggableElement {
+    constructor(snapPoints) {
+        this.snapPoints = snapPoints;
+    }
+
     dragMouseDown(e) {
         e.preventDefault();
-        document.onmouseup = this.closeDragElement.bind(this); // Bind the event handler to the instance
-        document.onmousemove = this.elementDrag.bind(this); // Bind the event handler to the instance
-        console.log("mousedown");
+        document.onmouseup = this.closeDragElement.bind(this);
+        document.onmousemove = this.elementDrag.bind(this)
     }
 
     elementDrag(e) {
         e.preventDefault();
         this.move(e.clientX, e.clientY);
-        console.log("mousemove");
     }
 
     closeDragElement() {
@@ -176,11 +231,23 @@ class draggableElement {
         document.onmousemove = null;
         this.snap();
     }
+
+    snap() {
+        let closestPoint = null;
+        let closestDistance = Infinity;
+        for (let point of this.snapPoints) {
+            if (Math.sqrt((this.polygon.getAttribute("points").split(" ")[0].split(",")[0] - point[0]) ** 2 + (this.polygon.getAttribute("points").split(" ")[0].split(",")[1] - point[1]) ** 2) < closestDistance) {
+                closestPoint = point;
+                closestDistance = Math.sqrt((this.polygon.getAttribute("points").split(" ")[0].split(",")[0] - point[0]) ** 2 + (this.polygon.getAttribute("points").split(" ")[0].split(",")[1] - point[1]) ** 2);
+            }
+        }
+        this.move(parseInt(closestPoint[0]) - this.polygon.getBBox().width / 2, parseInt(closestPoint[1]) - this.polygon.getBBox().width / 2);
+    }
 }
 
-class Robber extends draggableElement {
+/*class Robber extends draggableElement {
     constructor(radius, svg) {
-        super();
+        super(tiles);
         this.svg = svg;
         this.radius = radius;
 
@@ -199,63 +266,49 @@ class Robber extends draggableElement {
         this.robber.setAttribute("cx", x);
         this.robber.setAttribute("cy", y);
     }
-    snap() {
-        for (let tile of tiles) {
-            if (Math.sqrt((this.robber.getAttribute("cx") - tile.x) ** 2 + (this.robber.getAttribute("cy") - tile.y) ** 2) < tile.sideLength) {
-                this.move(tile.x, tile.y);
-                return;
-            }
-        }
-    }
-}
+}*/
 
 class Building extends draggableElement {
     constructor(x, y, color, type, svg) {
-        super();
+        super(vertices);
         this.x = x;
         this.y = y;
         this.color = color;
         this.type = type;
         this.svg = svg;
-        this.shape = `0,0 10,-10 20,0 20,20 0,20`;
-        if (this.type == "City") {
-            this.shape = `0,0 10,-10 20,0 20,10 40,10 40,30 0,30`;
+
+        switch (this.type) {
+            case "Settlement":
+                this.shape = `0,0 10,-10 20,0 20,20 0,20`;
+                break;
+            case "City":
+                this.shape = `0,0 10,-10 20,0 20,10 40,10 40,30 0,30`;
+                break;
         }
 
         this.draw();
     }
+
     draw() {
         this.polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
         this.polygon.setAttribute("fill", this.color);
         this.polygon.setAttribute("points", this.shape);
         this.move(this.x, this.y);
-        this.polygon.setAttribute("stroke", "black");
-        this.polygon.setAttribute("stroke-width", "2");
         this.polygon.onmousedown = this.dragMouseDown.bind(this); // Bind the event handler to the instance
         this.svg.appendChild(this.polygon);
     }
+
     move(x, y) {
         this.polygon.setAttribute("points", this.shape.split(" ").map(point => {
             let coords = point.split(",");
             return `${parseInt(coords[0]) + x},${parseInt(coords[1]) + y}`;
         }).join(" "));
     }
-    snap() {
-        let closestVertex = null;
-        let closestDistance = Infinity;
-        for (let vertex of vertices) {
-            if (Math.sqrt((this.polygon.getAttribute("points").split(" ")[0].split(",")[0] - vertex[0]) ** 2 + (this.polygon.getAttribute("points").split(" ")[0].split(",")[1] - vertex[1]) ** 2) < closestDistance) {
-                closestVertex = vertex;
-                closestDistance = Math.sqrt((this.polygon.getAttribute("points").split(" ")[0].split(",")[0] - vertex[0]) ** 2 + (this.polygon.getAttribute("points").split(" ")[0].split(",")[1] - vertex[1]) ** 2);
-            }
-        }
-        this.move(parseInt(closestVertex[0]) - this.polygon.getBBox().width / 2, parseInt(closestVertex[1]) - this.polygon.getBBox().width / 2);
-    }
 }
 
 let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 svg.setAttribute("width", "100%");
-svg.setAttribute("height", "1000");
+svg.setAttribute("height", "650");
 
 const resources = ["Brick", "Wool", "Ore", "Grain", "Lumber"];
 
@@ -270,13 +323,6 @@ const map = [
 const vertices = [];
 const tiles = drawMap(generateMap(5), svg);
 
-new Building(50, 100, "red", "Settlement", svg);
+let player = new Player("Alex", "red");
 
-new Building(50, 120, "blue", "Settlement", svg);
-
-new Building(100, 100, "lime", "Settlement", svg);
-
-new Building(100, 120, "purple", "City", svg);
-new Robber(20, svg);
-
-document.body.appendChild(svg);
+document.body.insertBefore(svg, document.body.firstChild);
