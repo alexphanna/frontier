0c2a0565fba5c4b10e4038718cace5f14c7f8cb2
Alex Hanna <ahanna0608@gmail.com>
2024-04-08 08:29:19 -0400

    Add rolling dice and token highlighting

--- script.js
+++ script.js
@@ -65,16 +65,28 @@ class Player {
 
 class Dice {
     roll() {
-        return Math.floor(Math.random() * 6) + 1 + Math.floor(Math.random() * 6) + 1;
+        const roll = Math.floor(Math.random() * 6) + 1 + Math.floor(Math.random() * 6) + 1;
+        document.getElementById("roll").innerHTML = `roll: ${roll}`;
+        for (let tile of map.tiles) {
+            if (tile.name == "Desert") {
+                continue;
+            }
+            if (roll == 7) {
+                tile.token.setAttribute("fill", "#ff4040");
+            }
+            else {
+                if (tile.number == roll) {
+                    tile.token.setAttribute("fill", "#00c0ff");
+                }
+                else {
+                    tile.token.setAttribute("fill", "#ffe0a0");
+                }
+            }
+        }
     }
 }
 
 class Map {
-    // Map:
-    // Jagged Array of integers representing resources
-    // Jagged Array of integers representing numbers
-    // Array of vertices
-    // Array of edges
     constructor (width) {
         // Resource allocation
         const resourceCounts = {
@@ -125,7 +137,8 @@ class Map {
     draw(svg, resources) {
         const sideLength = 75;
         const inradius = (Math.sqrt(3) / 2) * sideLength;
-        const tiles = [];
+        this.tiles = [];
+        this.tilesPoints = [];
     
         let maxLength = 0;
         for (let i = 0; i < this.resourceMap.length; i++) {
@@ -135,16 +148,18 @@ class Map {
         for (let i = 0; i < this.resourceMap.length; i++) {
             for (let j = 0; j < this.resourceMap[i].length; j++) {
                 if (this.resourceMap[i][j] == 6) {
-                    tiles.push(new Tile("Desert", "", 100 + inradius * 2 * j + (maxLength - this.resourceMap[i].length) * inradius, 100 + sideLength * 1.5 * i, sideLength, svg));
+                    this.tiles.push(new Tile("Desert", 7, 100 + inradius * 2 * j + (maxLength - this.resourceMap[i].length) * inradius, 100 + sideLength * 1.5 * i, sideLength, svg));
                 }
                 else {
-                    tiles.push(new Tile(resources[this.resourceMap[i][j]], this.numberMap[i][j], 100 + inradius * 2 * j + (maxLength - this.resourceMap[i].length) * inradius, 100 + sideLength * 1.5 * i, sideLength, svg));
+                    this.tiles.push(new Tile(resources[this.resourceMap[i][j]], this.numberMap[i][j], 100 + inradius * 2 * j + (maxLength - this.resourceMap[i].length) * inradius, 100 + sideLength * 1.5 * i, sideLength, svg));
                 }
+                this.tilesPoints.push([100 + inradius * 2 * j + (maxLength - this.resourceMap[i].length) * inradius, 100 + sideLength * 1.5 * i]);
             }
         }
 
+        // vertex = { x, y }
         this.vertices = [];
-        for (let tile of tiles) {
+        for (let tile of this.tiles) {
             for (let point of tile.hexagon.getAttribute("points").split(" ")) {
                 if (this.vertices.filter(vertex => vertex.join(",") == point).length == 0) {
                     this.vertices.push(point.split(","));
@@ -152,8 +167,9 @@ class Map {
             }
         }
 
+        // edge = { x, y, θ }
         this.edges = [];
-        for (let tile of tiles) {
+        for (let tile of this.tiles) {
             const hexagonPoints = tile.hexagon.getAttribute("points").split(" ");
             for (let i = 0; i < hexagonPoints.length; i++) {
                 const vertex1 = hexagonPoints[i].split(",");
@@ -213,22 +229,22 @@ class Tile {
         group.appendChild(this.hexagon);
 
         if (this.name != "Desert") {
-            let token = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-            token.setAttribute("cx", this.x);
-            token.setAttribute("cy", this.y);
-            token.setAttribute("r", this.sideLength / 3);
-            token.setAttribute("fill", "#ffe0a0");
-            token.setAttribute("stroke", "black");
-            token.setAttribute("stroke-width", "2");
-            group.appendChild(token);
+            this.token = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+            this.token.setAttribute("cx", this.x);
+            this.token.setAttribute("cy", this.y);
+            this.token.setAttribute("r", this.sideLength / 3);
+            this.token.setAttribute("fill", "#ffe0a0");
+            this.token.setAttribute("stroke", "black");
+            this.token.setAttribute("stroke-width", "2");
+            group.appendChild(this.token);
     
-            let text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-            text.setAttribute("x", this.x);
-            text.setAttribute("y", this.y);
-            text.setAttribute("fill", (this.number == 6 || this.number == 8 ? "red" : "black"));
-            text.setAttribute("font-size", "30");
-            text.textContent = this.number;
-            group.appendChild(text);
+            this.text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+            this.text.setAttribute("x", this.x);
+            this.text.setAttribute("y", this.y);
+            this.text.setAttribute("fill", (this.number == 6 || this.number == 8 ? "red" : "black"));
+            this.text.setAttribute("font-size", "30");
+            this.text.textContent = this.number;
+            group.appendChild(this.text);
         }
 
         this.svg.appendChild(group);
@@ -260,24 +276,38 @@ class draggableElement {
 
 class Robber extends draggableElement {
     constructor(radius, svg) {
-        super(tiles);
+        super();
+        this.snapPoints = map.tilesPoints;
         this.svg = svg;
         this.radius = radius;
 
         this.draw();
     }
     draw() {
-        this.robber = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        this.robber.setAttribute("cx", "100");
-        this.robber.setAttribute("cy", "100");
-        this.robber.setAttribute("r", this.radius);
-        this.robber.setAttribute("fill", "black");
-        this.robber.onmousedown = this.dragMouseDown.bind(this);
-        this.svg.appendChild(this.robber);
+        this.circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        this.circle.setAttribute("cx", "100");
+        this.circle.setAttribute("cy", "100");
+        this.circle.setAttribute("r", this.radius);
+        this.circle.setAttribute("fill", "black");
+        this.circle.onmousedown = this.dragMouseDown.bind(this);
+        this.svg.appendChild(this.circle);
     }
+
     move(x, y) {
-        this.robber.setAttribute("cx", x);
-        this.robber.setAttribute("cy", y);
+        this.circle.setAttribute("cx", x);
+        this.circle.setAttribute("cy", y);
+    }
+    
+    snap() {
+        let closestPoint = null;
+        let closestDistance = Infinity;
+        for (let point of this.snapPoints) {
+            if (Math.sqrt((this.circle.getAttribute("cx") - point[0]) ** 2 + (this.circle.getAttribute("cy") - point[1]) ** 2) < closestDistance) {
+                closestPoint = point;
+                closestDistance = Math.sqrt((this.circle.getAttribute("cx") - point[0]) ** 2 + (this.circle.getAttribute("cy") - point[1]) ** 2);
+            }
+        }
+        this.move(closestPoint[0] - this.circle.getAttribute("radius"), closestPoint[1] - this.circle.getAttribute("radius"));
     }
 }
 
@@ -328,7 +358,7 @@ class Building extends draggableElement {
                 closestDistance = Math.sqrt((this.polygon.getAttribute("points").split(" ")[0].split(",")[0] - point[0]) ** 2 + (this.polygon.getAttribute("points").split(" ")[0].split(",")[1] - point[1]) ** 2);
             }
         }
-        this.move(parseInt(closestPoint[0]) - this.polygon.getBBox().width / 2, parseInt(closestPoint[1]) - this.polygon.getBBox().width / 2);
+        this.move(closestPoint[0] - this.polygon.getBBox().width / 2, closestPoint[1] - this.polygon.getBBox().width / 2);
     }
 }
 
@@ -369,7 +399,7 @@ class Road extends draggableElement {
             }
         }
         this.rect.setAttribute("transform", `rotate(${closestPoint[2]})`);
-        this.move(parseInt(closestPoint[0]) - this.rect.getAttribute("width") / 2, parseInt(closestPoint[1]) - this.rect.getAttribute("height") / 2);
+        this.move(closestPoint[0] - this.rect.getAttribute("width") / 2, closestPoint[1] - this.rect.getAttribute("height") / 2);
     }
 }
 
@@ -411,7 +441,8 @@ let map = new Map(5);
 
 let player = new Player("Alex", "blue");
 
-let dice1 = new Dice();
-let dice2 = new Dice();
+let dice = new Dice();
+
+new Robber(20, svg);
 
 document.body.insertBefore(svg, document.body.firstChild);
