a759826d0a5af2f18512ad52f56bf55808a2d466
Alex Hanna <ahanna0608@gmail.com>
2024-07-09 17:52:24 -0400

    Add server edges

--- script.js
+++ script.js
@@ -57,46 +57,11 @@ class Map {
 
         // edge = { x, y, θ }
         this.edges = [];
-        let legalEdges = document.createElementNS("http://www.w3.org/2000/svg", "g");
-        legalEdges.setAttribute("id", "legalEdges");
+        ws.send('get edges');
         let edges = document.createElementNS("http://www.w3.org/2000/svg", "g");
         edges.setAttribute("id", "edges");
-        for (let tile of svg.getElementsByClassName("tile")) {
-            for (let hexagon of tile.getElementsByTagName("polygon")) {
-                let points = hexagon.getAttribute("points").trim().split(" ");
-                for (let i = 0; i < points.length; i++) {
-                    const vertex1 = points[i].split(",");
-                    const vertex2 = points[(i + 1) % points.length].split(",");
-                    let edge = [Math.min(vertex1[0], vertex2[0]) + Math.abs(vertex1[0] - vertex2[0]) / 2, Math.min(vertex1[1], vertex2[1]) + Math.abs(vertex1[1] - vertex2[1]) / 2];
-                    edge.push(Math.atan2(vertex1[1] - vertex2[1], vertex1[0] - vertex2[0]) * 180 / Math.PI);
-                    if (this.edges.filter(e => e.join(",") == edge.join(",")).length == 0) {
-                        this.edges.push(edge);
-
-                        let circle = createCircle(edge[0], edge[1]);
-                        circle.addEventListener('click', function () {
-                            legalEdges.setAttribute("visibility", "hidden");
-                            ws.send(`build road ${edge[0]} ${edge[1]} ${edge[2]} ${color}`);
-
-                            // add new legal edges
-                            for (let i = 0; i < edges.children.length; i++) {
-                                let circle = edges.children[i];
-                                let x = parseFloat(circle.getAttribute("cx"));
-                                let y = parseFloat(circle.getAttribute("cy"));
-                                if (Math.sqrt(Math.pow(x - edge[0], 2) + Math.pow(y - edge[1], 2)) <= this.sideLength * 1 && !legalEdges.contains(circle)) {
-                                    legalEdges.appendChild(circle);
-                                }
-                            }
-                            legalEdges.removeChild(circle);
-                        });
-                        edges.appendChild(circle);
-                    }
-                }
-            }
-        }
         svg.appendChild(edges);
         edges.setAttribute("visibility", "hidden");
-        svg.appendChild(legalEdges);
-        legalEdges.setAttribute("visibility", "hidden");
 
         center(svg);
     }
@@ -152,61 +117,83 @@ class Map {
 
         return { x, y };
     }
-}
-function createCircle(x, y) {
-    let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    circle.setAttribute("cx", x);
-    circle.setAttribute("cy", y);
-    circle.setAttribute("r", 1.5);
-    circle.setAttribute("fill", "transparent");
-    circle.setAttribute("stroke", "#ffffffc0");
-    circle.setAttribute("stroke-width", ".5");
-    circle.addEventListener('mouseover', function () {
-        circle.setAttribute("stroke", "#ffff00c0");
-    });
-    circle.addEventListener('mouseout', function () {
-        circle.setAttribute("stroke", "#ffffffc0");
-    });
-    return circle;
-}
+    edgeToStandard(x, y) {
+        var row = Math.floor((y - this.topMargin) / ((this.circumradius * 2 - this.sideLength) / 2 + this.sideLength) * 2);
+        if (row % 2 == 0) {
+            var col = (x - this.leftMargin - this.inradius / 2 - this.inradius * (5 - this.terrainMap[(row < 5 ? row : row - 2) / 2].length)) / this.inradius;
+        }
+        else {
+            var col = (x - this.leftMargin - this.inradius * (5 - this.terrainMap[Math.floor(row / 2)].length)) / (this.inradius * 2);
+        }
 
-function createRoad(x, y, angle, color) {
-    let road = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    road.class = "road";
-    road.setAttribute("fill", color);
-    road.setAttribute("width", "8");
-    road.setAttribute("height", "2");
-    road.setAttribute("x", x - road.getAttribute("width") / 2);
-    road.setAttribute("y", y - road.getAttribute("height") / 2);
-    road.setAttribute("transform", `rotate(${angle})`);
-    return road;
+        return { row, col };
+    }
+    standardToEdge(row, col) {
+        if (row % 2 == 0) {
+            var x = col * this.inradius + this.inradius * (5 - this.terrainMap[(row < 5 ? row : row - 2) / 2].length) + this.leftMargin + this.inradius / 2;
+            var y = Math.floor(row / 2) * ((this.circumradius * 2 - this.sideLength) / 2 + this.sideLength) + ((this.circumradius * 2 - this.sideLength) / 4) + this.topMargin;
+            var angle = (row < 5 ? (col % 2 == 0 ? -30 : 30) : (col % 2 == 0 ? 30 : -30));
+        }
+        else {
+            var x = col * (this.inradius * 2) + this.inradius * (5 - this.terrainMap[Math.floor(row / 2)].length) + this.leftMargin;
+            var y = Math.floor(row / 2) * (this.circumradius + this.sideLength / 2) + this.topMargin + this.circumradius;
+            var angle = 90;
+        }
+        return { x, y, angle };
+    }
 }
-
-function createBuilding(x, y, fill, stroke, type) {
-    switch (type) {
-        case "settlement":
-            var shape = `0,0 2,-2 4,0 4,4 0,4`;
-            var width = 2;
-            var height = 2;
-            break;
-        case "city":
-            var shape = `0,0 2,-2 4,0 4,2 8,2 8,6 0,6`;
-            var width = 4;
-            var height = 3;
-            break;
+class Building {
+    static shapes = {
+        settlement : `0,0 2,-2 4,0 4,4 0,4`,
+        city : `0,0 2,-2 4,0 4,2 8,2 8,6 0,6`
+    }
+    static widths = {
+        settlement : 2,
+        city : 4
+    }
+    static heights = {
+        settlement : 2,
+        city : 3
     }
 
-    let building = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-    building.setAttribute("class", type);
-    building.setAttribute("fill", fill);
-    building.setAttribute("stroke", stroke);
-    building.setAttribute("stroke-width", ".5");
-    building.setAttribute("points", shape);
-    building.setAttribute("points", shape.split(" ").map(point => {
-        let coords = point.split(",");
-        return `${parseInt(coords[0]) + x - width},${parseInt(coords[1]) + y - height}`;
-    }).join(" "));
-    return building;
+    static createRoad(x, y, angle, fill, stroke) {
+        let road = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        road.class = "road";
+        road.setAttribute("fill", fill);
+        road.setAttribute("width", "8");
+        road.setAttribute("height", "2");
+        road.setAttribute("stroke", stroke);
+        road.setAttribute("stroke-width", ".5");
+        road.setAttribute("x", x - road.getAttribute("width") / 2);
+        road.setAttribute("y", y - road.getAttribute("height") / 2);
+        road.setAttribute("transform", `rotate(${angle})`);
+        return road;
+    }
+
+    static createBuilding(x, y, fill, stroke, type) {
+        let building = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+        building.setAttribute("class", type);
+        building.setAttribute("fill", fill);
+        building.setAttribute("stroke", stroke);
+        building.setAttribute("stroke-width", ".5");
+        building.setAttribute("points", Building.pointToShape(x, y, type));
+        return building;
+    }
+
+    static pointToShape(x, y, type) {
+        let oldShape = Building.shapes[type].split(" ")
+        let shape = '';
+        for (let i = 0; i < oldShape.length; i++) {
+            let coords = oldShape[i].split(",");
+            shape += `${parseFloat(coords[0]) + x - Building.widths[type]},${parseFloat(coords[1]) + y - Building.heights[type]} `;
+        }
+        return shape;
+    }
+    static shapeToPoint(shape, type) {
+        let x = parseFloat(shape.split(" ")[0].split(",")[0]) + Building.widths[type];
+        let y = parseFloat(shape.split(" ")[0].split(",")[1]) + Building.heights[type];
+        return { x, y };
+    }
 }
 
 function createTile(x, y, width, terrain, number) {
@@ -295,25 +282,55 @@ function build(type) {
         vertices.setAttribute("visibility", "visible");
     }
     else {
-        let edges = svg.getElementById('legalEdges');
+        let edges = svg.getElementById('edges');
         edges.setAttribute("visibility", "visible");
     }
 }
 
-function update(players) {
+function updateUI(players) {
     let leftBar = document.getElementById('leftBar');
     leftBar.innerHTML = '';
     let rightBar = document.getElementById('rightBar');
     rightBar.innerHTML = '';
     for (let player of players) {
-        let header = document.createElement('h2');
-        header.style.color = color;
-        header.textContent = `${player.name} (${player.points})`;
+        let title = document.createElement('h2');
+        title.style.color = player.color;
+        title.textContent = `${player.name} (${player.points})`;
+        let resourcesHeading = document.createElement('h3');
+        
         if (player.name == name) {
-            leftBar.appendChild(header);
+            let buildingsHeading = document.createElement('h3');
+            buildingsHeading.textContent = 'Buildings:';
+            let buildings = document.createElement('ul');
+            for (let building in player.buildings) {
+                buildings.appendChild(document.createElement('li')).textContent = `${building.charAt(0).toUpperCase() + building.slice(1)}: ${player.buildings[building]}`;
+            }
+
+            resourcesHeading.textContent = 'Resources:';
+            let resources = document.createElement('ul');
+            for (let resource in player.resources) {
+                resources.appendChild(document.createElement('li')).textContent = `${resource.charAt(0).toUpperCase() + resource.slice(1)}: ${player.resources[resource]}`;
+            }
+
+            leftBar.appendChild(title);
+            leftBar.appendChild(document.createElement('br'));
+            leftBar.appendChild(buildingsHeading);
+            leftBar.appendChild(buildings);
+            leftBar.appendChild(document.createElement('br'));
+            leftBar.appendChild(resourcesHeading);
+            leftBar.appendChild(resources);
         }
         else {
-            rightBar.appendChild(header);
+            let resourceCount = 0;
+            for (let resource in player.resources) {
+                resourceCount += player.resources[resource];
+            }
+            resourcesHeading.textContent = `Resources: ${resourceCount}`;
+
+            rightBar.appendChild(title);
+            rightBar.appendChild(document.createElement('br'));
+            rightBar.appendChild(resourcesHeading);
+            rightBar.appendChild(document.createElement('br'));
         }
     }
 
@@ -337,21 +354,31 @@ function joinServer() {
 
         if (args[0] === 'players') {
             const players = JSON.parse(args[1]);
-            update(players);
+            updateUI(players);
         }
         else if (args[0] === 'build') {
-            const vertex = map.standardToVertex(parseInt(args[2]), parseInt(args[3]));
             if (args[1] === 'settlement' || args[1] === 'city') {
-                const building = createBuilding(vertex.x, vertex.y, args[4], "black", args[1]);
+                const vertex = map.standardToVertex(parseInt(args[2]), parseInt(args[3]));
+                const building = Building.createBuilding(vertex.x, vertex.y, args[4], "black", args[1]);
                 document.getElementById('buildings').appendChild(building);
             }
             else if (args[1] == 'city') {
-                const building = createBuilding(vertex.x, vertex.y, args[4], "black", args[1]);
-                let buildings = document.getElementById('buildings');
+                const vertex = map.standardToVertex(parseInt(args[2]), parseInt(args[3]));
+                const building = Building.createBuilding(vertex.x, vertex.y, args[4], "black", args[1]);
+                let buildings = svg.getElementById('buildings');
+                for (let i = 0; i < buildings.children.length; i++) {
+                    const point = vertexToStandard(Building.shapeToPoint(buildings.children[i].getAttribute("points")));
+                    console.log(point.x, parseInt(args[2]), point.y, parseInt(args[3]));
+                    if (Math.abs(point.x - parseInt(args[2])) < 1 && Math.abs(point.y - parseInt(args[3])) < 1 && buildings.children[i].classList.contains('settlement')) {
+                        buildings.removeChild(buildings.children[i]);
+                        break;
+                    }
+                }
                 buildings.appendChild(building);
             }
-            if (args[1] === 'road') {
-                const road = createRoad(vertex.x, vertex.y, parseFloat(args[4]), args[5]);
+            else if (args[1] === 'road') {
+                const edge = map.standardToEdge(parseInt(args[2]), parseInt(args[3]));
+                const road = Building.createRoad(edge.x, edge.y, args[4], args[5], "black");
                 document.getElementById('roads').appendChild(road);
             }
         }
@@ -373,13 +400,7 @@ function joinServer() {
                 for (let j = 0; j < legalVertices[i].length; j++) {
                     if (legalVertices[i][j]) {
                         const vertex = map.standardToVertex(i, j);
-                        /*let circle = createCircle(parseInt(vertex.x), parseInt(vertex.y));
-                        circle.addEventListener('click', function () {
-                            vertices.setAttribute("visibility", "hidden");
-                            ws.send(`build ${currentType} ${i} ${j} ${color}`);
-                        });
-                        vertices.appendChild(circle);*/
-                        let settlement = createBuilding(vertex.x, vertex.y, "transparent", "#ffffffc0", "settlement");
+                        let settlement = Building.createBuilding(vertex.x, vertex.y, "transparent", "#ffffffc0", "settlement");
                         settlement.addEventListener('click', function () {
                             vertices.setAttribute("visibility", "hidden");
                             ws.send(`build ${currentType} ${i} ${j} ${color}`);
@@ -391,6 +412,33 @@ function joinServer() {
                             settlement.setAttribute("stroke", "#ffffffc0");
                         });
                         vertices.appendChild(settlement);
+                    }
+                }
+            }
+        }
+        else if (args[0] === 'edges') {
+            var legalEdges = JSON.parse(args[1]);
+            var edges = svg.getElementById('edges');
+            edges.innerHTML = '';
+            for (let i = 0; i < legalEdges.length; i++) {
+                for (let j = 0; j < legalEdges[i].length; j++) {
+                    if (legalEdges[i][j]) {
+                        const edge = map.standardToEdge(i, j);
+                        const temp = map.edgeToStandard(edge.x, edge.y);
+                        console.log(i, temp.row, j, temp.col)
+
+                        let road = Building.createRoad(edge.x, edge.y, edge.angle, "transparent", "#ffffffc0");
+                        road.addEventListener('click', function () {
+                            edges.setAttribute("visibility", "hidden");
+                            ws.send(`build road ${i} ${j} ${edge.angle} ${color}`);
+                        });
+                        road.addEventListener('mouseover', function () {
+                            road.setAttribute("stroke", "#ffff00c0");
+                        });
+                        road.addEventListener('mouseout', function () {
+                            road.setAttribute("stroke", "#ffffffc0");
+                        });
+                        edges.appendChild(road);
                     }
                 }
             }
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ class Player {
         this.color = color;
         this.ws = ws;
         this.points = 0;
+        this.buildings = {
+            settlements: 5,
+            cities: 4,
+            roads: 15
+        }
         this.resources = {
             brick: 0,
             grain: 0,
@@ -77,7 +82,21 @@ function broadcastVertices() {
                 vertices[j].push(cityVertices[j][k] == i);
             }
         }   
+        const playerEdges = [];
+        for (let j = 0; j < edges.length; j++) {
+            playerEdges.push([]);
+            for (let k = 0; k < edges[j].length; k++) {
+                playerEdges[j].push(edges[j][k] == i);
+            }
+        }
         Array.from(players)[i].ws.send('vertices city ' + JSON.stringify(vertices));
+        Array.from(players)[i].ws.send('edges ' + JSON.stringify(playerEdges));
+    }
+}
+
+function addEdge(row, col) {
+    if (isNaN(edges[row][col])) {
+        edges[row][col] = turn % players.size;
     }
 }
 
@@ -87,6 +106,7 @@ let players = new Set();
 let map = generateMap();
 let turn = 0;
 
+// vertices
 /*  x  x [0, 0, 0, 0, 0, 0, 0] x  x
  *  x [0, 0, 0, 0, 0, 0, 0, 0, 0] x
  * [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -109,6 +129,45 @@ for (let i = Math.ceil(map.terrainMap.length / 2) - 1; i >= 0; i--) {
     cityVertices.push(Array.from(temp2));
 }
 
+// edges
+/* 0:  [0, 0, 0, 0, 0, 0]
+ * 1:  [0, 0, 0, 0]
+ * 2:  [0, 0, 0, 0, 0, 0, 0, 0]
+ * 3:  [0, 0, 0, 0, 0]
+ * 4:  [0, 0, 0, r, r, 0, 0, 0, 0, 0]
+ * 5:  [0, 0, X, 0, 0, 0]
+ * 6:  [0, 0, 0, r, r, 0, 0, 0, 0, 0]
+ * 7:  [0, 0, 0, 0, 0]
+ * 8:  [0, 0, 0, 0, 0, 0, 0, 0]
+ * 9:  [0, 0, 0, 0]
+/* 10: [0, 0, 0, 0, 0, 0]
+ */
+
+/* NaN = no road
+ * 0 = player 0... able to build
+ * player.size = road
+ */
+
+let edges = [];
+for (let i = Math.ceil(map.terrainMap.length / 2) - 1; i >= 0; i--) {
+    let temp1 = [];
+    let temp2 = [];
+    for (let j = 0; j < map.terrainMap[i].length * 2; j++) {
+        if (j < map.terrainMap[i].length + 1) {
+            temp1.push(NaN);
+        }
+        temp2.push(NaN);
+    }
+    edges.push(Array.from(temp1));
+    if (i != Math.ceil(map.terrainMap.length / 2) - 1) {
+        edges.unshift(Array.from(temp1));
+    }
+    edges.push(Array.from(temp2));
+    edges.unshift(Array.from(temp2));
+}
+
+console.log(edges);
+
 wss.on('connection', (ws) => {
     console.log('connected');
 
@@ -125,42 +184,86 @@ wss.on('connection', (ws) => {
             }
         }
         else if (args[0] === 'build') {
+            let player = Array.from(players)[turn % players.size];
             const row = parseInt(args[2]);
             const col = parseInt(args[3]);
-            if ((args[1] === 'settlement' && settlementVertices[row][col]) || (args[1] === 'city' && cityVertices[row][col] === turn % players.size)) {
-                Array.from(players)[turn % players.size].points++;
+            if ((args[1] === 'settlement' && settlementVertices[row][col] && player.buildings["settlements"] > 0)) {
                 broadcast(String(message));
+                player.points++;
+                player.buildings["settlements"]--;
+                settlementVertices[row][col] = false;
+                cityVertices[row][col] = turn % players.size;
+                for (let i = (col == 0 ? 0 : -1); i <= (col == settlementVertices[row].length - 1 ? 0 : 1); i++) {
+                    settlementVertices[row][col + i] = false;
+                    edges[row * 2][col + (i > 0 ? 0 : -1)] = turn % players.size;
+                }
 
-                if (args[1] === 'settlement') {
-                    settlementVertices[row][col] = false;
-                    cityVertices[row][col] = turn % players.size;
-                    for (let i = (col == 0 ? 0 : -1); i <= (col == settlementVertices[row].length - 1 ? 0 : 1); i++) {
-                        settlementVertices[row][col + i] = false;
+                if (((row <= 2 && col % 2) || (row >= 3 && !(col % 2))) && row > 0) {
+                    if (settlementVertices[row].length != settlementVertices[row - 1].length) {
+                        settlementVertices[row - 1][col + (row >= Math.floor(settlementVertices.length / 2) ? 1 : -1)] = false;
                     }
-
-                    if (((row <= 2 && col % 2) || (row >= 3 && !(col % 2))) && row > 0) {
-                        if (settlementVertices[row].length != settlementVertices[row - 1].length) {
-                            settlementVertices[row - 1][col + (row >= Math.floor(settlementVertices.length / 2) ? 1 : -1)] = false;
-                        }
-                        else {
-                            settlementVertices[row - 1][col] = false;
-                        }
-                    }
-                    else if (row < settlementVertices.length - 1) {
-                        if (settlementVertices[row].length != settlementVertices[row + 1].length) {
-                            settlementVertices[row + 1][col + (row >= Math.floor(settlementVertices.length / 2) ? -1 : 1)] = false;
-                        }
-                        else {
-                            settlementVertices[row + 1][col] = false;
-                        }
+                    else {
+                        settlementVertices[row - 1][col] = false;
                     }
                 }
-                else if (args[1] === 'city') {
-                    cityVertices[row][col] = NaN;
+                else if (row < settlementVertices.length - 1) {
+                    if (settlementVertices[row].length != settlementVertices[row + 1].length) {
+                        settlementVertices[row + 1][col + (row >= Math.floor(settlementVertices.length / 2) ? -1 : 1)] = false; 
+                    }
+                    else {
+                        settlementVertices[row + 1][col] = false;
+                    }
                 }
-                broadcastVertices();
-                broadcast('players ' + JSON.stringify(Array.from(players))); // increases points
+
+                if (row <= 2 && row > 0) {
+                    edges[row * 2 + (col % 2 == 1 ? -1 : 1)][Math.floor(col / 2)] = turn % players.size;
+                }
+                else if (row >= 3 && row < settlementVertices.length - 1) {
+                    edges[row * 2 + (col % 2 == 1 ? 1 : -1)][Math.floor(col / 2)] = turn % players.size;
+                }
             }
+            else if (args[1] === 'city' && cityVertices[row][col] === turn % players.size && player.buildings["cities"] > 0) {
+                broadcast(String(message));
+                player.points++;
+                player.buildings["cities"]--;
+                player.buildings["settlements"]++;
+                cityVertices[row][col] = NaN;
+            } 
+            else if (args[1] === 'road' && edges[row][col] == turn % players.size && player.buildings["roads"] > 0) {
+                broadcast(String(message));
+                player.buildings["roads"]--;
+                if (row % 2 == 0) {
+                    for (let i = (col == 0 ? 0 : -1); i <= (col == edges[row].length - 1 ? 0 : 1); i++) {
+                        if (isNaN(edges[row][col + i])) {
+                            edges[row][col + i] = turn % players.size;
+                        }
+                    }
+                    
+                    addEdge(row + (col % 2 == 0 ? 1 : -1), row < 5 ? Math.floor(col / 2) : Math.ceil(col / 2));
+                    addEdge(row + (col % 2 == 0 ? -1 : 1), row < 5 ? Math.ceil(col / 2) : Math.floor(col / 2))
+                }
+                else {
+                    for (let i = -1; i <= 1; i+= 2) {
+                        if (row < 5) {
+                            addEdge(row + i, col * 2);
+                            addEdge(row + i, col * 2 + i);
+                        }
+                        else if (row == 5) {
+                            addEdge(row + i, col * 2 - 1);
+                            addEdge(row + i, col * 2);
+                        }
+                        else  if (row > 5) {
+                            addEdge(row + i, col * 2);
+                            addEdge(row + i, col * 2 - i);
+                        }
+                    }
+                }
+                edges[row][col] = players.size;
+                console.log(edges);
+            }
+                
+            broadcastVertices();
+            broadcast('players ' + JSON.stringify(Array.from(players)));
         }
         else if (args[0] === 'get') {
             if (args[1] === 'map') {
@@ -173,6 +276,9 @@ wss.on('connection', (ws) => {
                 else if (args[2] === "city") {
                     ws.send('vertices city ' + JSON.stringify(cityVertices));
                 }
+            }
+            else if (args[1] === 'edges') {
+                ws.send('edges ' + JSON.stringify(edges));
             }
         }
         else if (args[0] === 'end') {
